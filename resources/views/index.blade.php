9b082d0351ebaa3158d0b800b2ab4ef71fceb4e7
MoralesMaria08 <ticsmorac@gmail.com>
2020-04-08 03:00:56 +0200

    correction des liens du menu

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,74 +4,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #333;
-                font-family: 'Lato', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            header {
-                border-top: 3px #F7C64A solid;
-                box-shadow: 0 6px 48px rgba(69, 69, 69, 0.08);
-                text-align: center;
-                padding:25px;
-                font-size: large;
-            }
-            .margium {
-                display: block;
-                margin: 0px auto;
-                max-width: 1000px;
-                position: inherit;
-            }
-            h1 {
-                font-size: xx-large;
-            }
-            .incitation a {
-                padding: 9px 25px;
-                background-color: #f5f5f5;
-                color: #333;
-                text-decoration: none;
-                margin:5px;
-                border-radius: 3px;
-            }
-            a.plein {
-                background-color: #F7C64A ;
-            }
-            .vide {
-                height: 80px;
-            }
-            #presentation {
-                background-image:url("{{ asset('img/premiere.jpg') }}");
-                background-size: cover;
-                background-position: center;
-                position: relative;
-                padding: 60px 0px;
-            }
-            #presentation h1{
-                color: #fff;
-                max-width: 620px;
-            }
-            #presentation p{
-                color: #fff;
-                max-width: 520px;
-                margin-bottom: 50px;
-                line-height: 175%;
-            }
-            .surcouche {
-                position: absolute;
-                top: 0; bottom: 0px ; left: 0; right: 0;
-                background-color: rgba(0, 0, 0, 0.6)
-            }
-        </style>
+       
     </head>
     <body>
         <header>Guide touristique d'Espagne</header>
