8df4dc11fd72b9783ff1182f3b1e5b49bc4bab9a
lucas lacroix <lacroixlucas59@gmail.com>
2020-04-10 12:16:46 +0200

    page register + verfiy

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,9 @@ Route::get('/a-propos', 'PremierControleur@APropos'); /* Page à propos */
 Route::get('/ag/{id}', 'PremierControleur@AG')->where('id', '[0-9]+'); /* Page d'un audioguide */
 Route::get('/ag', 'PremierControleur@AGListe'); /* Page de la liste d'audioguide */
 Auth::routes();
-
+/*
 Route::get('/home', 'HomeController@index')->name('home');
+*/
+Route::get('/home', function () {
+    return redirect('/');
+});
