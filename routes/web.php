9d11f14f29fe38d643e5cd6eb9469ba5296cc6d5
lucas lacroix <lacroixlucas59@gmail.com>
2020-04-10 11:30:19 +0200

    début de dev de l'auth

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,6 @@ Route::get('/', 'PremierControleur@index');  /* Page d'accueil */
 Route::get('/a-propos', 'PremierControleur@APropos'); /* Page à propos */
 Route::get('/ag/{id}', 'PremierControleur@AG')->where('id', '[0-9]+'); /* Page d'un audioguide */
 Route::get('/ag', 'PremierControleur@AGListe'); /* Page de la liste d'audioguide */
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
