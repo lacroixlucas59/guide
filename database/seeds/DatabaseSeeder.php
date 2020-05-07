e48108af1e2a5c0f6cd788a3190cb96a19015c0b
lucas lacroix <lacroixlucas59@gmail.com>
2020-05-07 16:49:05 +0200

    modification du seeder nouveau audio et nouvelles images

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,38 +14,52 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
 
         DB::table('ag')->insert([
-            'titre' => Str::random(15),
-            'image' => "img/premiere.jpg",
-            'audio' => 'teste.mp3',
-            'contenu' => Str::random(105),     
+            'titre' => "Madrid",
+            'image' => "d/madrid.jpg",
+            'audio' => 'd/andalousie.mp3',
+            'contenu' => "Proinde die funestis interrogationibus praestituto imaginarius  adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim  cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.",     
         ]);
 
         DB::table('ag')->insert([
-            'titre' => Str::random(15),
-            'image' => "img/premiere.jpg",
-            'audio' => 'teste.mp3',
-            'contenu' => Str::random(105),     
+            'titre' => "Séville",
+            'image' => "d/seville.jpg",
+            'audio' => 'd/espagne.mp3',
+            'contenu' => "Iam quae essent agenda praedoctis, et adsistebant hinc inde notarii,  cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.",     
         ]);
 
         DB::table('ag')->insert([
-            'titre' => Str::random(15),
-            'image' => "img/premiere.jpg",
-            'audio' => 'teste.mp3',
-            'contenu' => Str::random(105),     
+            'titre' => "Barcelone",
+            'image' => "d/barcelone.jpg",
+            'audio' => 'd/andalousie.mp3',
+            'contenu' => "Adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.",     
         ]);
 
         DB::table('ag')->insert([
-            'titre' => Str::random(15),
-            'image' => "img/premiere.jpg",
-            'audio' => 'teste.mp3',
-            'contenu' => Str::random(105),     
+            'titre' => "Ibiza",
+            'image' => "d/ibiza.jpg",
+            'audio' => 'd/espagne.mp3',
+            'contenu' => "Praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, exsertantis aurem subinde per aulaeum",     
         ]);
 
         DB::table('ag')->insert([
-            'titre' => Str::random(15),
-            'image' => "img/premiere.jpg",
-            'audio' => 'teste.mp3',
-            'contenu' => Str::random(105),     
+            'titre' => "Grenade",
+            'image' => "d/grenade.jpg",
+            'audio' => 'd/andalousie.mp3',
+            'contenu' => "Equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.",     
+        ]);
+
+        DB::table('ag')->insert([
+            'titre' => "Bilbao",
+            'image' => "d/bilbao.jpg",
+            'audio' => 'd/espagne.mp3',
+            'contenu' => "Aredt Praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, exsertantis aurem subinde per aulaeum",     
+        ]);
+
+        DB::table('ag')->insert([
+            'titre' => "Saragosse",
+            'image' => "d/saragosse.jpg",
+            'audio' => 'd/andalousie.mp3',
+            'contenu' => "Aredt Praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, exsertantis aurem subinde per aulaeum",     
         ]);
     }
 }
