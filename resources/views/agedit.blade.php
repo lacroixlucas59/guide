d2999cfcda9dea47e733708c5b4c19b843804aab
lucas lacroix <lacroixlucas59@gmail.com>
2020-05-11 15:22:43 +0200

    interface : mise en page et icon supprimer

--- resources/views/agedit.blade.php
+++ resources/views/agedit.blade.php
@@ -12,13 +12,18 @@
                 </div>
             </div> 
         </section>
-        <div class="margium">
-            <div class='liste'>
+        <div class="margium-m">
+            <div class='listedit'>
                 @foreach ($agl as $agl)
-                <a href="ag/{{ $agl -> id }}">
-                    <img class='visuelvote' src='../{{ $agl -> image }}' alt='illustration'/>
-                    <span>{{ $agl -> titre }}</span>
-                </a>
+                <div class="elementedit">
+                    <a href="ag/{{ $agl -> id }}">
+                        <img class='visuelvote' src='../{{ $agl -> image }}' alt='illustration'/>
+                        <span>{{ $agl -> titre }}</span>
+                    </a>
+                    <a href="#nan">
+                         <img class="action" src='../{{ 'img/supprimer.svg' }}' alt="#supp"/>
+                    </a>
+                </div>
                 @endforeach
             </div>
         </div> 
